d7c7260d2f8a6ef686b3aad0e732bdd3d90547c7
slowingwand28 <hum24004@byui.edu>
2025-01-31 18:17:00 -0700

    continued work on cool pics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,6 +7,8 @@ menuButton.addEventListener("click", toggleMenu);
 
 window.addEventListener("resize", handleResize);
 
+document.querySelector(".gallery").addEventListener("click", viewHandler);
+
 function toggleMenu() {
     if (menu.classList.contains("hide")) {
         menu.classList.remove("hide");
@@ -26,5 +28,34 @@ function handleResize() {
         }
     }
 }
+
+function viewerTemplate(pic, alt) {
+    return `
+    <div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+    </div>
+    `
+}
+
+function viewHandler(event) {
+	// create a variable to hold the element that was clicked on from event.target
+    const pic = event.target;
+	// get the src attribute from that element and 'split' it on the "-"
+    const source = pic.getAttribute("src");
+    const sourceSplit = source.split("-");
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const newSource = `${sourceSplit[0]}-full.jpeg`;
+	// insert the viewerTemplate into the top of the body element
+	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSource, "Enlarged Image"));
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
+
 
 handleResize();
