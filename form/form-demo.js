c4d16810cb24fd2e1072113599d9455eab2b1cd6
slowingwand28 <hum24004@byui.edu>
2025-02-18 10:01:47 -0700

    began working on week 7 ponder practice

---
+++ form/form-demo.js
@@ -0,0 +1,23 @@
+function togglePaymentDetails(e) {
+    // get a reference to the form. We can access all the named form inputs through the form element.
+    const theForm = document.querySelector("#checkoutForm");
+    // we will also need the creditCardContainer and paypalUsernameContainer
+    const creditCardContainer = document.querySelector("#creditCardNumberContainer");
+    const paypalContainer = document.querySelector("#paypalUsernameContainer");
+  
+    // Hide payment containers by adding the '.hide' class to each of them
+  
+    // Disable required for payment fields...if we hide a required field the browser will throw an error when we try to submit!
+
+    // element.required=false
+    // element.classList.add("hide")
+  
+    // Show the container based on the selected payment method, and add the required attribute back.
+    let value = e.target.value;
+    
+    if (value == "creditCard")
+}
+
+// attach a change event handler to the paymentMethod input
+// attach a submit event handler to the form
+document.querySelector("#paymentMethod").addEventListener("change", togglePaymentDetails);
