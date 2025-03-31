4953cedbff6a0e9ab71b11027504d9b95557d426
slowingwand28 <hum24004@byui.edu>
2025-03-31 10:02:25 -0600

    began to create game array in JS

---
+++ game-stats/game.js
@@ -0,0 +1,26 @@
+const games = [
+    {
+        title: "Apex Legends",
+        developer: "Respawn Entertainment",
+        publisher: "Electronic Arts",
+        image: "/images/apex.jpg",
+        description: "Apex Legends is the award-winning, free-to-play Hero Shooter from Respawn Entertainment. Master an ever-growing roster of legendary characters with powerful abilities, and experience strategic squad play and innovative gameplay in the next evolution of Hero Shooter and Battle Royale.",
+        tags: ["Multiplayer", "FPS", "Sci-Fi"]
+    },
+    {
+        title: "Baldur’s Gate 3",
+        developer: "Larian Studios",
+        publisher: "Larian Studios",
+        image: "/images/bg3.jpg",
+        description: "Baldur’s Gate 3 is a story-rich, party-based RPG set in the universe of Dungeons & Dragons, where your choices shape a tale of fellowship and betrayal, survival and sacrifice, and the lure of absolute power.",
+        tags: ["Single-player", "Multiplayer", "RPG", "Fantasy", "Open World"]
+    },
+    {
+        title: "Elden Ring",
+        developer: "FromSoftware",
+        publisher: "Bandai Namco Entertainment",
+        image: "/images/elden_ring.jpg",
+        description: "THE CRITICALLY ACCLAIMED FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
+        tags: ["Single-player", "RPG", "Fantasy", "Open World"]
+    }
+]
