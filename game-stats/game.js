9289634993a2d9bd13a659004a9548cf0a15a15d
slowingwand28 <hum24004@byui.edu>
2025-03-31 21:51:18 -0600

    added rest of the games to the JS file

--- game-stats/game.js
+++ game-stats/game.js
@@ -22,5 +22,30 @@ const games = [
         image: "/images/elden_ring.jpg",
         description: "THE CRITICALLY ACCLAIMED FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
         tags: ["Single-player", "RPG", "Fantasy", "Open World"]
+    },
+    {
+        title: "Halo: The Master Chief Collection",
+        developer: "Halo Studios and Bungie Inc.",
+        publisher: "Xbox Game Studios",
+        image: "/images/halo_mcc.png",
+        description: "The Master Chief’s iconic journey includes six games, built for PC and collected in a single integrated experience. Whether you’re a long-time fan or meeting Spartan 117 for the first time, The Master Chief Collection is the definitive Halo gaming experience.",
+        tags: ["Single-player", "Multiplayer", "FPS", "Sci-Fi"]
+    },
+    {
+        title: "Stellaris",
+        developer: "Paradox Development Studio",
+        publisher: "Paradox Interactive",
+        image: "/images/stellaris.png",
+        description: "Explore a galaxy full of wonders in this sci-fi grand strategy game from Paradox Development Studios. Interact with diverse alien races, discover strange new worlds with unexpected events and expand the reach of your empire. Each new adventure holds almost limitless possibilities.",
+        tags: ["Single-player", "Multiplayer", "Strategy", "Sci-Fi"]
+    },
+    {
+        title: "Total War: WARHAMMER III",
+        developer: "Creative Assembly",
+        publisher: "SEGA",
+        image: "/images/tww3.jpg",
+        description: "The cataclysmic conclusion to the Total War: WARHAMMER trilogy is here. Rally your forces and step into the Realm of Chaos, a dimension of mind-bending horror where the very fate of the world will be decided. Will you conquer your Daemons… or command them?",
+        tags: ["Single-player", "Multiplayer", "Strategy", "Fantasy"]
     }
 ]
+
