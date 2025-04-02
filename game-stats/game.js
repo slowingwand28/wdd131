37e44dd14a852fe2c0544fa947f4f6ba72352abc
slowingwand28 <hum24004@byui.edu>
2025-04-02 10:03:25 -0600

    began work on homepage game viewer

--- game-stats/game.js
+++ game-stats/game.js
@@ -3,7 +3,7 @@ const games = [
         title: "Apex Legends",
         developer: "Respawn Entertainment",
         publisher: "Electronic Arts",
-        image: "/images/apex.jpg",
+        image: "images/apex.jpg",
         description: "Apex Legends is the award-winning, free-to-play Hero Shooter from Respawn Entertainment. Master an ever-growing roster of legendary characters with powerful abilities, and experience strategic squad play and innovative gameplay in the next evolution of Hero Shooter and Battle Royale.",
         tags: ["Multiplayer", "FPS", "Sci-Fi"]
     },
@@ -11,7 +11,7 @@ const games = [
         title: "Baldur’s Gate 3",
         developer: "Larian Studios",
         publisher: "Larian Studios",
-        image: "/images/bg3.jpg",
+        image: "images/bg3.jpg",
         description: "Baldur’s Gate 3 is a story-rich, party-based RPG set in the universe of Dungeons & Dragons, where your choices shape a tale of fellowship and betrayal, survival and sacrifice, and the lure of absolute power.",
         tags: ["Single-player", "Multiplayer", "RPG", "Fantasy", "Open World"]
     },
@@ -19,7 +19,7 @@ const games = [
         title: "Elden Ring",
         developer: "FromSoftware",
         publisher: "Bandai Namco Entertainment",
-        image: "/images/elden_ring.jpg",
+        image: "images/elden_ring.jpg",
         description: "THE CRITICALLY ACCLAIMED FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.",
         tags: ["Single-player", "RPG", "Fantasy", "Open World"]
     },
@@ -27,7 +27,7 @@ const games = [
         title: "Halo: The Master Chief Collection",
         developer: "Halo Studios and Bungie Inc.",
         publisher: "Xbox Game Studios",
-        image: "/images/halo_mcc.png",
+        image: "images/halo_mcc.png",
         description: "The Master Chief’s iconic journey includes six games, built for PC and collected in a single integrated experience. Whether you’re a long-time fan or meeting Spartan 117 for the first time, The Master Chief Collection is the definitive Halo gaming experience.",
         tags: ["Single-player", "Multiplayer", "FPS", "Sci-Fi"]
     },
@@ -35,7 +35,7 @@ const games = [
         title: "Stellaris",
         developer: "Paradox Development Studio",
         publisher: "Paradox Interactive",
-        image: "/images/stellaris.png",
+        image: "images/stellaris.png",
         description: "Explore a galaxy full of wonders in this sci-fi grand strategy game from Paradox Development Studios. Interact with diverse alien races, discover strange new worlds with unexpected events and expand the reach of your empire. Each new adventure holds almost limitless possibilities.",
         tags: ["Single-player", "Multiplayer", "Strategy", "Sci-Fi"]
     },
@@ -43,7 +43,7 @@ const games = [
         title: "Total War: WARHAMMER III",
         developer: "Creative Assembly",
         publisher: "SEGA",
-        image: "/images/tww3.jpg",
+        image: "images/tww3.jpg",
         description: "The cataclysmic conclusion to the Total War: WARHAMMER trilogy is here. Rally your forces and step into the Realm of Chaos, a dimension of mind-bending horror where the very fate of the world will be decided. Will you conquer your Daemons… or command them?",
         tags: ["Single-player", "Multiplayer", "Strategy", "Fantasy"]
     }
